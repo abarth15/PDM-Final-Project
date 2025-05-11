7550ab7625844eb541b256b1dae79f83e6bdb69c
Annika Barth <annika.barth5@gmail.com>
2025-05-11 10:49:28 -0400

    Removed install.packages from the script.

--- PJM_Queue_Analysis.R
+++ PJM_Queue_Analysis.R
@@ -1,6 +1,3 @@
-install.packages("readxl")
-install.packages("httr")
-install.packages("gt")
 
 library(magrittr)
 library(dplyr)
@@ -60,7 +57,7 @@ fuel_percent_energy <- filter(pjm_full_cycle, Status == "Active") %>%
   arrange(desc(`Percent of MW`))
 
 gt(fuel_percent_energy) %>%
-  tabe_header(title = "Fuel Type as Percent of Total MW")
+  tab_header(title = "Fuel Type as Percent of Total MW")
 
 # Calculate MW of energy in the queue per state and county and select top
 # five states and counties for solar, storage, wind, and hyrbid resources
@@ -74,7 +71,8 @@ top_three_states <- filter(pjm_full_cycle, Status == "Active" & Fuel %in%
   arrange(desc(`Clean Energy MW`)) %>%
   head(3)
 
-gt(top_three_states)
+gt(top_three_states) %>%
+  tab_header(title = "Top Three States for Clean Energy in PJM's Queue")
 
 top_five_counties <- filter(pjm_full_cycle, Status == "Active" & Fuel %in% 
                               c("Solar", "Wind", "Storage", "Solar,Storage,Hybrid")) %>% 
@@ -85,7 +83,8 @@ top_five_counties <- filter(pjm_full_cycle, Status == "Active" & Fuel %in%
   arrange(desc(`Clean Energy MW`)) %>%
   head(5)
 
-gt(top_five_counties)
+gt(top_five_counties) %>%
+  tab_header(title = "Tope Five Counties for Clean Energy in PJM's Queue")
 
 # Calculate and graph average length of time that in-service projects spent
 # in the interconnection queue between 2005 and 2025 by fuel type
